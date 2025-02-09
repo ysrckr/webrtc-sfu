60ac92d10d9d51943afacaaff8108e251cf71a15
Yasar Cakir <dev.yasarcakir@gmail.com>
2025-02-09 18:13:08 +0700

    Add newWorker method in WebRTC class

--- src/lib/webrtc/index.ts
+++ src/lib/webrtc/index.ts
@@ -1,13 +1,24 @@
-import { TLS } from "@/types/server";
+import { createWorker, types } from "mediasoup";
+
+import { MediasoupTLS } from "@/types/mediasoup";
 
 export class WebRTC {
   private static _instance: WebRTC;
-  private _tls: TLS;
-  private constructor(tls: TLS) {
+  private _tls: MediasoupTLS;
+
+  private constructor(tls: MediasoupTLS) {
     this._tls = tls;
   }
 
-  public static Instance(tls: TLS) {
+  public static Instance(tls: MediasoupTLS) {
     return this._instance || (this._instance = new this(tls));
+  }
+
+  public async newWorker(logLevel: types.WorkerLogLevel) {
+    return await createWorker({
+      logLevel,
+      dtlsCertificateFile: this._tls.cert,
+      dtlsPrivateKeyFile: this._tls.key,
+    });
   }
 }
---
+++ src/types/mediasoup/index.ts
@@ -0,0 +1,6 @@
+export type MediasoupTLS = {
+  key: string;
+  cert: string;
+};
+
+
