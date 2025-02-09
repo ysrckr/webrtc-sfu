ae0474891a605fbe0cdb1683c25a6206902678a6
Yasar Cakir <dev.yasarcakir@gmail.com>
2025-02-09 21:57:26 +0700

    Add _logLevel and _workers to WebRTC class

--- src/lib/webrtc/index.ts
+++ src/lib/webrtc/index.ts
@@ -5,20 +5,26 @@ import { MediasoupTLS } from "@/types/mediasoup";
 export class WebRTC {
   private static _instance: WebRTC;
   private _tls: MediasoupTLS;
+  private _logLevel: types.WorkerLogLevel;
+  private _workers: types.Worker[];
 
-  private constructor(tls: MediasoupTLS) {
+  private constructor(tls: MediasoupTLS, logLevel: types.WorkerLogLevel) {
     this._tls = tls;
+    this._logLevel = logLevel;
+    this._workers = [];
   }
 
-  public static Instance(tls: MediasoupTLS) {
-    return this._instance || (this._instance = new this(tls));
+  public static Instance(tls: MediasoupTLS, logLevel: types.WorkerLogLevel) {
+    return this._instance || (this._instance = new this(tls, logLevel));
   }
 
-  public async newWorker(logLevel: types.WorkerLogLevel) {
-    return await createWorker({
-      logLevel,
+  public async newWorker() {
+    const worker = await createWorker({
+      logLevel: this._logLevel,
       dtlsCertificateFile: this._tls.cert,
       dtlsPrivateKeyFile: this._tls.key,
     });
+
+    this._workers.push(worker);
   }
 }
